17413c25b7efcfb48a9559e7594a1300858406db
David Hurrell <dhurrell@hotmail.co.uk>
2023-10-07 09:52:42 +0100

    migrate db, add home page

---
+++ calculator/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"

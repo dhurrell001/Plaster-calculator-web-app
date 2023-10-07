187cbd6f6821030393190a2a9a0c37b6e483270c
David Hurrell <dhurrell@hotmail.co.uk>
2023-10-07 10:26:50 +0100

    Added unit tests
    added unit test to homepage and about page. Check if urls work and
    correct html page is retrieved.

--- calculator/urls.py
+++ calculator/urls.py
@@ -5,6 +5,6 @@ from .views import HomePageView, AboutPageView
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
-    path("", HomePageView.as_view(), name="index"),
+    path("", HomePageView.as_view(), name="home"),
 
 ]

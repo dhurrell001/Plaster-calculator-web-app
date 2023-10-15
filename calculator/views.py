1467087c82c106bdeb8fb4aef17a3b4a5b3b3f9a
David Hurrell <dhurrell@hotmail.co.uk>
2023-10-15 14:00:28 +0100

    update forms
    Add new results form. Add buttons for PDF files. Add changes to
    view function to update results form

--- calculator/views.py
+++ calculator/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
-from .forms import PlasterCalculatorForm
+from .forms import PlasterCalculatorForm, PlasterResultForm
 from .models import Plaster
 from decimal import Decimal
 import math
@@ -9,6 +9,10 @@ import math
 
 def bagsNeeded(kg, bagWeight):
     return math.ceil(kg / bagWeight)
+
+
+def CalculateArea(length, width):
+    return length * width
 
 
 def index(request):
@@ -25,39 +29,93 @@ class AboutPageView(TemplateView):
 
 def plaster_calculator(request):
     template_name = 'home.html'
-    plaster_amount = None
-    plaster_description = None
-    bags_needed = None
+    plaster_description = ''
+    total_metres = 0
 
     if request.method == 'POST':
-        form = PlasterCalculatorForm(request.POST)
-        if form.is_valid():
-            plasterType = form.cleaned_data['plasterType']
-            length = form.cleaned_data['length']
-            width = form.cleaned_data['width']
-            thickness = form.cleaned_data['thickness']
-            # Convert coverage_by_metre to Decimal
+        plaster_form = PlasterCalculatorForm(request.POST)
+
+        if plaster_form.is_valid():
+            plasterType = plaster_form.cleaned_data['plasterType']
+            length = plaster_form.cleaned_data['length']
+            width = plaster_form.cleaned_data['width']
+            thickness = plaster_form.cleaned_data['thickness']
+
             coverage_kg_per_mm_per_metre = Decimal(
                 str(plasterType.coverage_kg_per_mm_per_metre))
-
-            # Convert length and width to Decimal
             length_decimal = Decimal(str(length))
             width_decimal = Decimal(str(width))
 
-            # Perform division
             total_metres = (length_decimal * width_decimal)
             plaster_amount = (
                 total_metres * coverage_kg_per_mm_per_metre) * thickness
-            bags_needed = bagsNeeded(plaster_amount, plasterType.plasterweight)
             plaster_description = plasterType.description
+
+            # Calculate bags needed
+            if plaster_amount and plasterType.plasterweight:
+                bags_needed = bagsNeeded(
+                    plaster_amount, plasterType.plasterweight)
+
+            # Create a PlasterResultForm instance and populate it with the results
+            result_form = PlasterResultForm({
+                'plaster_amount': plaster_amount,
+                'plaster_description': plaster_description,
+                'bags_needed': bags_needed,
+                'total_area': total_metres
+
+
+            })
+
     else:
-        form = PlasterCalculatorForm()
+        plaster_form = PlasterCalculatorForm()
+        result_form = PlasterResultForm()
 
     context = {
-        'form': form,
-        'plaster_amount': plaster_amount,
+        'plaster_form': plaster_form,
+        'result_form': result_form,
         'plaster_description': plaster_description,
-        'bags_needed': bags_needed
+        'total_area': total_metres
+
     }
 
     return render(request, template_name, context)
+
+
+# def plaster_calculator(request):
+#     template_name = 'home.html'
+#     plaster_amount = None
+#     plaster_description = None
+#     bags_needed = None
+
+#     if request.method == 'POST':
+#         form = PlasterCalculatorForm(request.POST)
+#         if form.is_valid():
+#             plasterType = form.cleaned_data['plasterType']
+#             length = form.cleaned_data['length']
+#             width = form.cleaned_data['width']
+#             thickness = form.cleaned_data['thickness']
+#             # Convert coverage_by_metre to Decimal
+#             coverage_kg_per_mm_per_metre = Decimal(
+#                 str(plasterType.coverage_kg_per_mm_per_metre))
+
+#             # Convert length and width to Decimal
+#             length_decimal = Decimal(str(length))
+#             width_decimal = Decimal(str(width))
+
+#             # Perform division
+#             total_metres = (length_decimal * width_decimal)
+#             plaster_amount = (
+#                 total_metres * coverage_kg_per_mm_per_metre) * thickness
+#             bags_needed = bagsNeeded(plaster_amount, plasterType.plasterweight)
+#             plaster_description = plasterType.description
+#     else:
+#         form = PlasterCalculatorForm()
+
+#     context = {
+#         'form': form,
+#         'plaster_amount': plaster_amount,
+#         'plaster_description': plaster_description,
+#         'bags_needed': bags_needed
+#     }
+
+#     return render(request, template_name, context)

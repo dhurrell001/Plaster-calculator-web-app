5238e4348c725935328bc2ad7c69fa78de78c559
David Hurrell <dhurrell@hotmail.co.uk>
2023-10-21 11:17:39 +0100

    Changed form display, add pdf view

--- calculator/urls.py
+++ calculator/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from . import views
 
 # from . import views
 from .views import HomePageView, AboutPageView
@@ -10,6 +11,7 @@ urlpatterns = [
 
     path('plaster-image/<int:plaster_id>/',
          display_plaster_image, name='display_plaster_image'),
+    path('view-pdf/<int:plaster_id>/', views.view_pdf, name='view_pdf'),
     # path('plaster-pdf/<int:plaster_id>/',
     #      display_plaster_pdf, name='display_plaster_pdf'),
 
